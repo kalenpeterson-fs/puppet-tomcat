24c90820f9190e67743a1df0a632d294cc91e495
Kalen <kpeterson@forsythe.com>
2016-09-29 10:17:55 -0700

    Example is working

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,46 @@
 #
 # Copyright 2016 Your name here, unless otherwise noted.
 #
-class tomcat {
+class tomcat (
+  $server_port    = '8005',
+  $connector_port = '8080',
+){
 
+  # Set the Module Path
+  $module_path  = '/etc/puppetlabs/code/modules'
 
+  # Install the tomcat package
+  package { 'tomcat7':
+    ensure => 'installed',
+    before => File['/etc/tomcat7/server.xml'],
+  }
+
+  # Manage the tomcat service
+  service { 'tomcat7':
+    ensure  => 'running',
+    require => Package['tomcat7'],
+  }
+
+  # Manage the server.xml file
+  file { '/etc/tomcat7/server.xml':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    notify  => Service['tomcat7'],
+    content => epp('tomcat/server.xml.epp', {
+      'server_port'    => $server_port,
+      'connector_port' => $connector_port,
+    }),
+  }
+
+  # Manage the content of the website
+  file { '/var/lib/tomcat7/webapps/ROOT/index.html':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['tomcat7'],
+    source  => "${module_path}/tomcat/files/index.html",
+  }
 }
